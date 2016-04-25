abe0484ce73acc58c50eb1946a33f422089f6985
Nate Parrish <nate.parrish@arrowpointe.com>
2016-04-24 21:48:59 -0700

    Add eslint, browserify, and first few steps of tutorial

---
+++ public/scripts/index.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CommentBox = require('./components/CommentBox.jsx');
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
